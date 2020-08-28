7ba7d8bcd83612230f7c1113e9dd985361ed8a6f
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2020-08-28 16:25:30 +0000

    initial commit

---
+++ infrastructure/main.tf
@@ -0,0 +1,33 @@
+locals {
+    env_prefix = "${var.shortcode}-${var.product}-${var.environment}-${var.location_short_code}"
+    env_prefix_no_separator = "${var.shortcode}${var.product}${var.environment}${var.location_short_code}"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.env_prefix}-rg"
+  location = "${var.location}"
+
+  tags = {
+    environment = "${var.environment}"
+    product = "${var.product}"        
+  }
+}
+
+resource "azurerm_storage_account" "static_storage" {
+  name                     = "${local.env_prefix_no_separator}stor"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  enable_https_traffic_only = true
+
+  static_website {
+    index_document = "index.html"
+  }
+
+  tags = {
+    environment = "${var.environment}"
+    product = "${var.product}"
+  }
+}
---
+++ infrastructure/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+    required_version = "~> 0.12.29"
+}
+
+provider "azurerm" {
+    version = "~>2.2.0"
+    features {}
+}
